023710034097c7dd2e863bb781ba1b25918a28f1
ObrigadoDeus <th_sola@hotmail.com>
2024-01-29 23:56:00 +0100

    Tests with gdextension

--- Tortas-cpp/InputBuffer.cpp
+++ Tortas-cpp/InputBuffer.cpp
@@ -1,5 +1,7 @@
 #include "InputBuffer.h"
 #include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/classes/input_map.hpp>
+#include <godot_cpp/variant/utility_functions.hpp>
 
 using namespace godot;
 
@@ -13,19 +15,47 @@ void InputBuffer::_bind_methods() {
 
 InputBuffer::InputBuffer() {
 	// Initialize any variables here.
-    buffer.reserve(InputBufferConsts::bufferSize);
 }
 
 InputBuffer::~InputBuffer() {
 	// Add your cleanup here.
 }
 
-void InputBuffer::_input(const Ref<InputEvent> &event)
+void InputBuffer::_ready()
 {
-    
+    set_process_priority(0);
+
+    //Fill the buffer
+    for(int i = 0; i < InputBufferConsts::bufferSize; ++i)
+    {
+        buffer.emplace_back(InputElement());
+    }
+}
+
+void InputBuffer::_unhandled_input(const Ref<InputEvent> &event)
+{    
+    //Add InputElemnts to the buffer
+    const auto& allActions = InputMap::get_singleton()->get_actions();    
+    for(int i = 0; i < allActions.size(); ++i)
+    {
+        const StringName& currentAction = allActions[i];
+        UtilityFunctions::print(currentAction);
+        if(event->is_action(allActions[i]))
+        {
+            buffer.emplace_front(InputElement(*(event.ptr()), currentAction));
+            
+        }
+    }
+
+    //Send signal to    
 }
 
 void InputBuffer::_process(double delta)
 {
-    //this->_input
+   // UtilityFunctions::print("--------------------------------");
+    for(InputElement& input : buffer)
+    {
+        //UtilityFunctions::print(input.getAction());
+    }
+
 }
--- Tortas-cpp/InputBuffer.h
+++ Tortas-cpp/InputBuffer.h
@@ -3,12 +3,40 @@
 
 #include <godot_cpp/classes/node.hpp>
 #include <godot_cpp/classes/input_event.hpp>
+#include <queue>
 
 namespace godot {
 
 struct InputElement
 {
-	InputEvent input;
+	public:
+	InputElement() = default;
+	InputElement(const InputEvent& event, const StringName& eventAction) : 
+	action(eventAction),
+	device(event.get_device()),
+	pressed(event.is_action_pressed(eventAction)),
+	released(event.is_action_released(eventAction)),
+	value(event.get_action_strength(eventAction))
+	{
+
+	}
+
+	int getDevice() const { return device; }
+	const StringName& getAction() const { return action; }
+	bool isAction(const StringName& act) const { return action == act; }
+	bool isPressed() const { return pressed; }
+	bool isReleased() const { return released; }
+	double getValue() const { return value; }
+
+	bool isProcessed() const { return processed; }
+
+	private:
+	int device = 0;
+	StringName action = "";
+	bool pressed = false;
+	bool released = false;
+	double value = 0.0f;
+
 	bool processed = false;
 };
 
@@ -25,10 +53,12 @@ public:
 	InputBuffer();
 	~InputBuffer();
 
-	void _input(const Ref<InputEvent>& event) override;
+    void _ready() override;
+	void _unhandled_input(const Ref<InputEvent>& event) override;
 	void _process(double delta) override;
 
-	std::vector<InputElement> buffer;
+	//std::queue<InputElement> buffer;
+	std::deque<InputElement> buffer;
 };
 
 }
